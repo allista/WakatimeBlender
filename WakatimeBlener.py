90874b19986fdf62034f5880a7d21c5050ff6fc6
Allis Tauri <allista@gmail.com>
2016-06-15 01:19:16 +0300

    First working release.

--- WakatimeBlener.py
+++ WakatimeBlener.py
@@ -1,5 +1,5 @@
 import bpy
-from bpy.api.handlers import persistent
+from bpy.app.handlers import persistent
 from bpy.props import StringProperty
 
 import json
@@ -7,24 +7,39 @@ import os
 import sys
 import time
 import threading
+from urllib import request
+from zipfile import ZipFile
 from configparser import ConfigParser
 from subprocess import Popen, STDOUT, PIPE
-import queue
+from queue import Queue, Empty
 
 __version__ = '1.0.0'
 
 bl_info = \
     {
         "name":     "Wakatime plugin for Blender",
-        "category": "Development"
+        "category": "Development",
+        "author":   "Allis Tauri <allista@gmail.com>",
+        "version": (1, 0),
+        "blender": (2, 77, 0),
+        "description": "Submit your working stats to Wakatime online service.",
+        "warning": "Beta",
+        # "tracker_url": "https://developer.blender.org/maniphest/task/create/?project=3&type=Bug",
     }
 
 # globals
+HEARTBEAT_FREQUENCY = 2
+_heartbeats = Queue()
+_hb_processor = None
+_last_hb = None
+_filename = ''
+
 PLUGIN_DIR = os.path.dirname(os.path.realpath(__file__))
-API_CLIENT = os.path.join(PLUGIN_DIR, 'packages', 'wakatime', 'cli.py')
-SETTINGS_FILE = 'config.ini'
+API_CLIENT_URL = 'https://github.com/wakatime/wakatime/archive/master.zip'
+API_CLIENT = os.path.join(PLUGIN_DIR, 'wakatime-master', 'wakatime', 'cli.py')
+SETTINGS_FILE = os.path.join(PLUGIN_DIR, 'wakatime.ini')
 SETTINGS = None
-ActivityHandler = None
+settings = None
 
 # Log Levels
 DEBUG   = 'DEBUG'
@@ -41,106 +56,54 @@ except ImportError:
 
 
 def u(text):
-    if text is None:
-        return None
+    if text is None: return None
     if isinstance(text, bytes):
-        try:
-            return text.decode('utf-8')
+        try: return text.decode('utf-8')
         except:
-            try:
-                return text.decode(sys.getdefaultencoding())
-            except:
-                pass
-    try:
-        return str(text)
-    except:
-        return text
+            try: return text.decode(sys.getdefaultencoding())
+            except: pass
+    try: return str(text)
+    except: return text
 
 
 def log(lvl, message, *args, **kwargs):
-    if lvl == DEBUG and not SETTINGS.get('debug'):
-        return
+    if lvl == DEBUG and not settings.getboolean('debug'): return
     msg = message
-    if len(args) > 0:
-        msg = message.format(*args)
-    elif len(kwargs) > 0:
-        msg = message.format(**kwargs)
+    if len(args) > 0: msg = message.format(*args)
+    elif len(kwargs) > 0: msg = message.format(**kwargs)
     print('[WakaTime] [{lvl}] {msg}'.format(lvl=lvl, msg=msg))
 
 
-def create_default_config():
-    """Creates the .wakatime.cfg INI file in $HOME directory, if it does
-    not already exist."""
-    if SETTINGS.has_section('settings'): return
-    SETTINGS.add_section('settings')
-    SETTINGS.set('settings', 'debug', False)
-    SETTINGS.set('settings', 'hidefilenames', False)
-
-
 class API_Key_Dialog(bpy.types.Operator):
-    bl_idname = "object.wakatime_api_key_dialog"
+    bl_idname = "ui.wakatime_api_key_dialog"
     bl_label = "Enter WakaTime API Key"
     api_key = StringProperty(name="API Key")
     default_key = ''
 
     def execute(self, context):
         if self.api_key:
-            SETTINGS.set('settings', 'api_key', str(self.api_key))
+            settings['api_key'] = str(self.api_key)
             save_settings()
         return {'FINISHED'}
 
     def invoke(self, context, event):
-        self.api_key = SETTINGS.get('settings', 'api_key', fallback='')
+        self.api_key = settings.get('api_key', fallback='')
         return context.window_manager.invoke_props_dialog(self)
 
 
-def prompt_api_key():
-    create_default_config()
-    try:
-        configs = parseConfigFile()
-        if configs is not None:
-            if configs.has_option('settings', 'api_key'):
-                API_Key_Dialog.default_key = configs.get('settings', 'api_key')
-    except: pass
-    if SETTINGS.has_option('settings', 'api_key'): return True
-    bpy.ops.object.wakatime_api_key_dialog('INVOKE_DEFAULT')
-    return False
-
-
-def save_settings():
-    with open(SETTINGS_FILE, 'w') as out:
-        SETTINGS.write(out)
-
-
-class Heartbeat(object):
-    def __init__(self, filename, timestamp, is_write):
-        self.filename  = filename
-        self.timestamp = timestamp
-        self.is_write  = is_write
-
-
-class SendHeartbeatsThread(threading.Thread):
-    """Non-blocking thread for sending heartbeats to api.
-    """
-
+class HeartbeatsSender(object):
     def __init__(self, heartbeat):
         threading.Thread.__init__(self)
-
-        self.debug   = SETTINGS.get('settings', 'debug')
-        self.api_key = SETTINGS.get('settings', 'api_key', fallback='')
-        self.ignore  = SETTINGS.get('settings', 'ignore', fallback=[])
-
+        self.debug   = settings.getboolean('debug')
+        self.api_key = settings.get('api_key', fallback='')
+        self.ignore  = settings.get('ignore', fallback=[])
         self.heartbeat = heartbeat
         self.has_extra_heartbeats = False
         self.extra_heartbeats = []
 
     def add_extra_heartbeats(self, extra_heartbeats):
-        self.has_extra_heartbeats = True
+        self.has_extra_heartbeats = len(extra_heartbeats) > 0
         self.extra_heartbeats = extra_heartbeats
-
-    def run(self):
-        """Running in background thread."""
-        self.send_heartbeats()
 
     @staticmethod
     def obfuscate_apikey(command_list):
@@ -154,18 +117,18 @@ class SendHeartbeatsThread(threading.Thread):
             cmd[apikey_index] = 'XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXX' + cmd[apikey_index][-4:]
         return cmd
 
-    def send_heartbeats(self):
-        ua = 'blender/%s blender-wakatime/%s' % (bpy.app.version_string, __version__)
+    def send(self):
+        ua = '"blender/%s blender-wakatime/%s"' % (bpy.app.version_string, __version__)
         cmd = [
-            sys.executable,
+            bpy.app.binary_path_python,
             API_CLIENT,
-            '--entity', self.heartbeat.filename,
-            '--time', str('%f' % self.heartbeat.timestamp),
+            '--entity', self.heartbeat['entity'],
+            '--time', str('%f' % self.heartbeat['timestamp']),
             '--plugin', ua,
         ]
         if self.api_key:
             cmd.extend(['--key', str(bytes.decode(self.api_key.encode('utf8')))])
-        if self.heartbeat.is_write:
+        if self.heartbeat['is_write']:
             cmd.append('--write')
         for pattern in self.ignore:
             cmd.extend(['--ignore', pattern])
@@ -174,8 +137,7 @@ class SendHeartbeatsThread(threading.Thread):
         if self.has_extra_heartbeats:
             cmd.append('--extra-heartbeats')
             stdin = PIPE
-            extra_heartbeats = [self.build_heartbeat(**x) for x in self.extra_heartbeats]
-            extra_heartbeats = json.dumps(extra_heartbeats)
+            extra_heartbeats = json.dumps(self.extra_heartbeats)
         else:
             extra_heartbeats = None
             stdin = None
@@ -191,7 +153,7 @@ class SendHeartbeatsThread(threading.Thread):
             output = u(output)
             retcode = process.poll()
             if (not retcode or retcode == 102) and not output:
-                log(INFO, 'OK')
+                log(DEBUG, 'OK')
             else:
                 log(ERROR, 'Error')
             if retcode:
@@ -202,77 +164,131 @@ class SendHeartbeatsThread(threading.Thread):
             log(ERROR, u(sys.exc_info()[1]))
 
 
-class WakaStats(object):
-    HEARTBEAT_FREQUENCY = 2
+class HeartbeatQueueProcessor(threading.Thread):
+    def __init__(self, q):
+        threading.Thread.__init__(self)
+        self.daemon = True
+        self._queue = q
 
+    def run(self):
+        while True:
+            time.sleep(1)
+            if 'api_key' not in settings: continue
+            try: heartbeat = self._queue.get_nowait()
+            except Empty: continue
+            if heartbeat is None: return
+            has_extra_heartbeats = False
+            extra_heartbeats = []
+            try:
+                while True:
+                    extra_heartbeats.append(self._queue.get_nowait())
+                    has_extra_heartbeats = True
+            except Empty: pass
+            sender = HeartbeatsSender(heartbeat)
+            if has_extra_heartbeats:
+                sender.add_extra_heartbeats(extra_heartbeats)
+            sender.send()
+
+
+class DownloadWakatime(threading.Thread):
     def __init__(self):
-        self._heartbeats = queue()
-        self._last_hb = None
-        self._filename = ''
-        bpy.app.handlers.load_post.append(self.load_handler)
-        bpy.app.handlers.save_pre.append(self.save_handler)
-        bpy.app.handlers.scene_update_post.append(self.activity_handler)
+        threading.Thread.__init__(self)
+        self.daemon = True
 
-    def __del__(self):
-        bpy.app.handlers.load_post.remove(self.load_handler)
-        bpy.app.handlers.save_pre.remove(self.save_handler)
-        bpy.app.handlers.scene_update_post.remove(self.activity_handler)
+    def run(self):
+        log(INFO, 'WakatimeBlender is registered')
+        if not os.path.isfile(API_CLIENT):
+            log(INFO, 'Downloading Wakatime...')
+            zip_file = os.path.join(PLUGIN_DIR, 'wakatime-master.zip')
+            request.urlretrieve(API_CLIENT_URL, zip_file)
+            log(INFO, 'Extracting Wakatime...')
+            with ZipFile(zip_file) as zf: zf.extractall(PLUGIN_DIR)
+            try: os.remove(zip_file)
+            except: pass
+            log(INFO, 'Finished extracting Wakatime.')
+        else: log(INFO, 'Found Wakatime client')
 
-    @persistent
-    def load_handler(self, dummy):
-        self._filename = bpy.data.filepath
-        self.handle_activity()
 
-    @persistent
-    def save_handler(self, dummy):
-        self.handle_activity(is_write=True)
+def save_settings():
+    with open(SETTINGS_FILE, 'w') as out:
+        SETTINGS.write(out)
 
-    @persistent
-    def activity_handler(self, dummy):
-        self.handle_activity()
 
-    def enough_time_passed(self, now, is_write):
-        return self._last_hb is None or now - self._last_hb.timestamp > 2 if is_write else self.HEARTBEAT_FREQUENCY * 60
+def setup():
+    global SETTINGS, settings, _hb_processor
+    download = DownloadWakatime()
+    download.start()
+    SETTINGS = ConfigParser()
+    SETTINGS.read(os.path.join(PLUGIN_DIR, SETTINGS_FILE))
+    if not SETTINGS.has_section('settings'):
+        log(INFO, 'Creating default config...')
+        SETTINGS.add_section('settings')
+        SETTINGS.set('settings', 'debug', str(False))
+        SETTINGS.set('settings', 'hidefilenames', str(False))
+    settings = SETTINGS['settings']
+    try:
+        configs = parseConfigFile()
+        if configs is not None:
+            if configs.has_option('settings', 'api_key'):
+                API_Key_Dialog.default_key = configs.get('settings', 'api_key')
+                log(INFO, 'Found default API key.')
+    except: pass
+    _hb_processor = HeartbeatQueueProcessor(_heartbeats)
+    _hb_processor.start()
 
-    def handle_activity(self, is_write=False):
-        timestamp = time.time()
-        last_file = self._last_hb.filename if self._last_hb is not None else ''
-        if self._filename != last_file or self.enough_time_passed(timestamp, is_write):
-            self._last_hb = Heartbeat(self._filename, timestamp, is_write)
-            self._heartbeats.put_nowait(self._last_hb)
 
-    def process_queue(self):
-        try:
-            heartbeat = self._heartbeats.get_nowait()
-        except queue.Empty:
-            return
+@persistent
+def load_handler(dummy):
+    global _filename
+    _filename = bpy.data.filepath
+    handle_activity()
 
-        has_extra_heartbeats = False
-        extra_heartbeats = []
-        try:
-            while True:
-                extra_heartbeats.append(self._heartbeats.get_nowait())
-                has_extra_heartbeats = True
-        except queue.Empty:
-            pass
 
-        thread = SendHeartbeatsThread(heartbeat)
-        if has_extra_heartbeats:
-            thread.add_extra_heartbeats(extra_heartbeats)
-        thread.start()
+@persistent
+def save_handler(dummy):
+    global _filename
+    _filename = bpy.data.filepath
+    handle_activity(is_write=True)
+
+
+@persistent
+def activity_handler(dummy):
+    handle_activity()
+
+
+def enough_time_passed(now, is_write):
+    return _last_hb is None or (now - _last_hb['timestamp'] > (2 if is_write else HEARTBEAT_FREQUENCY * 60))
+
+
+def handle_activity(is_write=False):
+    global _last_hb
+    timestamp = time.time()
+    last_file = _last_hb['entity'] if _last_hb is not None else ''
+    if _filename and (_filename != last_file or enough_time_passed(timestamp, is_write)):
+        _last_hb = {'entity':_filename, 'timestamp':timestamp, 'is_write':is_write}
+        _heartbeats.put_nowait(_last_hb)
 
 
 def register():
-    global SETTINGS, ActivityHandler
-    SETTINGS = ConfigParser.read(os.path.join(PLUGIN_DIR, SETTINGS_FILE))
     log(INFO, 'Initializing WakaTime plugin v%s' % __version__)
     bpy.utils.register_module(__name__)
-    bpy.utils.register_class(API_Key_Dialog)
-    ActivityHandler = WakaStats()
-    prompt_api_key()
+    bpy.app.handlers.load_post.append(load_handler)
+    bpy.app.handlers.save_post.append(save_handler)
+    bpy.app.handlers.scene_update_post.append(activity_handler)
+    setup()
 
 
 def unregister():
-    global ActivityHandler
-    ActivityHandler = None
     save_settings()
+    log(INFO, 'Unregistering WakaTime plugin v%s' % __version__)
+    _heartbeats.put_nowait(None)
+    _heartbeats.task_done()
+    _hb_processor.join()
+    bpy.app.handlers.load_post.remove(load_handler)
+    bpy.app.handlers.save_post.remove(save_handler)
+    bpy.app.handlers.scene_update_post.remove(activity_handler)
+    bpy.utils.unregister_module(__name__)
+
+
+if __name__ == '__main__':
+    register()
