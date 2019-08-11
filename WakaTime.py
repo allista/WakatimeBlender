324e824cdaebf09b965bb9bae04f4ce3dcc79f8e
CycloneMagnum <mai.k@live.com>
2019-08-12 00:29:56 +0800

    Update the blender2.80 API, delete deprecated code

--- WakaTime.py
+++ WakaTime.py
@@ -1,8 +1,8 @@
 import bpy
 from bpy.app.handlers import persistent
 from bpy.props import StringProperty
-from bpy.utils import register_class
-from bpy.utils import unregister_class
+
+
 
 import json
 import os
@@ -23,7 +23,7 @@ bl_info = \
         "category":    "Development",
         "author":      "Allis Tauri <allista@gmail.com>",
         "version":     (1, 0, 1),
-        "blender":     (2, 77, 0),
+        "blender":     (2, 80, 0),
         "description": "Submits your working stats to the Wakatime time tracking service.",
         "warning":     "Beta",
         "tracker_url": "https://github.com/allista/WakatimeBlender/issues",
@@ -75,7 +75,7 @@ def log(lvl, message, *args, **kwargs):
 class API_Key_Dialog(bpy.types.Operator):
     bl_idname = "ui.wakatime_api_key_dialog"
     bl_label  = "Enter WakaTime API Key"
-    api_key   = StringProperty(name="API Key")
+    api_key   : StringProperty(name="API Key")
     is_shown  = False
 
     @classmethod
@@ -245,26 +245,15 @@ def handle_activity(is_write=False):
         _last_hb = {'entity':_filename, 'timestamp':timestamp, 'is_write':is_write}
         _heartbeats.put_nowait(_last_hb)
 
-classes = (
-    API_Key_Dialog,
-    HeartbeatQueueProcessor,
-    DownloadWakatime,
-)
-register, unregister = bpy.utils.register_classes_factory(classes)
-
 def register():
     global  REGISTERED
     log(INFO, 'Initializing WakaTime plugin v%s' % __version__)
     setup()
-
-    for cls in classes:
-        register_class(cls)
-
+    bpy.utils.register_class(API_Key_Dialog)
     bpy.app.handlers.load_post.append(load_handler)
     bpy.app.handlers.save_post.append(save_handler)
-    bpy.app.handlers.scene_update_post.append(activity_handler)
+    bpy.app.handlers.depsgraph_update_pre.append(activity_handler)
     REGISTERED = True
-
 
 def unregister():
     global REGISTERED
@@ -273,11 +262,8 @@ def unregister():
     REGISTERED = False
     bpy.app.handlers.load_post.remove(load_handler)
     bpy.app.handlers.save_post.remove(save_handler)
-    bpy.app.handlers.scene_update_post.remove(activity_handler)
-    
-    for cls in classes:
-        unregister_class(cls)
-
+    bpy.app.handlers.depsgraph_update_pre.remove(activity_handler)
+    bpy.utils.unregister_class(API_Key_Dialog)
     _heartbeats.put_nowait(None)
     _heartbeats.task_done()
     _hb_processor.join()
